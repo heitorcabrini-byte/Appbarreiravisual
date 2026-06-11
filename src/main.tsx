e448149f00dde36df451053c34a54b20cd15b9a1
heitorcabrini-byte <heitorcabrini@gmail.com>
2026-06-10 23:24:20 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './app/App'; // <--- Ajustado para a pasta real
 import './styles/main.css'; // <--- ESSA LINHA PRECISA ESTAR AQUI!
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
