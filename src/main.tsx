c5e708cd328088577ae1c9f5fa0cc28b57bf4a24
heitorcabrini-byte <heitorcabrini@gmail.com>
2026-06-10 23:21:52 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './styles/main.css'; // <--- ESSA LINHA PRECISA ESTAR AQUI!
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
-  import '../default_shadcn_theme.css';
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
