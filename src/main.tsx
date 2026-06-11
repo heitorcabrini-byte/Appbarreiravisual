2848eb3fda1ba793f4aa93965bcffc5aa20b6b8a
heitorcabrini-byte <heitorcabrini@gmail.com>
2026-06-10 23:24:37 -0300

    Fix import statement for main.css in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App'; // <--- Ajustado para a pasta real
-import './styles/main.css'; // <--- ESSA LINHA PRECISA ESTAR AQUI!
+import './styles/main.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
